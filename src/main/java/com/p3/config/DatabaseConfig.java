1f5364fc94b8b0d84ab54b3e2ca055fef87e9483
Sakarias <49249574+SakariasM@users.noreply.github.com>
2024-11-17 19:53:17 +0100

    Added Server functionality
    Added new generic classes for server handling and replaced most of former login methods

--- src/main/java/com/p3/config/DatabaseConfig.java
+++ src/main/java/com/p3/config/DatabaseConfig.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConfig {
     private static final String URL = "jdbc:mysql://localhost:3306/database";
     private static final String USER = "root";
-    private static final String PASSWORD = "1234"; //IMPORTANT: USE YOUR OWN PASSWORD
+    private static final String PASSWORD = "test"; //IMPORTANT: USE YOUR OWN PASSWORD
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
---
+++ src/main/java/com/p3/networking/ServerApi.java
@@ -0,0 +1,66 @@
+package com.p3.networking;
+
+import java.net.URI;
+import java.net.http.*;
+import java.util.Map;
+
+public class ServerApi {
+    private final String BASE_URL = "http://localhost:8080/api/";  // TODO ændrer til server ip
+
+    /*
+    * Requests take path efter BASE_URL
+    * */
+    public HttpResponse<String> get(String path, Map<String, String> headers) {     // Can take headers but for now we dont have any paths with them
+        HttpRequest.Builder builder = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + path))
+                .GET();
+
+        if (headers != null){
+            headers.forEach(builder::header);   // For each optional header argument, add to url
+        }
+        return ServerApiUtil.execReq(builder.build());
+    }
+
+    public HttpResponse<String> post(String path, Map<String, String> headers, String body) throws Exception {
+        HttpRequest.Builder builder = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + path))
+                .header("Content-Type", "application/json") // Set content type so server know types of
+                .POST(HttpRequest.BodyPublishers.ofString(body));
+
+        if (headers != null){
+            headers.forEach(builder::header);
+        }
+
+        return ServerApiUtil.execReq(builder.build());
+    }
+
+    public HttpResponse<String> put(String path, Map<String, String> headers, String body) throws Exception {
+        HttpRequest.Builder builder = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + path))
+                .header("Content-Type", "application/json");
+
+        if (headers != null){
+            headers.forEach(builder::header);
+        }
+
+        if (body != null && !body.isEmpty()){
+            builder.PUT(HttpRequest.BodyPublishers.ofString(body));
+        } else {
+            builder.PUT(HttpRequest.BodyPublishers.noBody());
+        }
+
+        return ServerApiUtil.execReq(builder.build());
+    }
+
+    public HttpResponse<String> delete(String path, Map<String, String> headers) throws Exception {
+        HttpRequest.Builder builder = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + path))
+                .DELETE();
+
+        if (headers != null){
+            headers.forEach(builder::header);
+        }
+
+        return ServerApiUtil.execReq(builder.build());
+    }
+}
